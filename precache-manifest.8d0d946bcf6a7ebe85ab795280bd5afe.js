ceec6bde2a65aa7ef1f6ffa776187b500222d0bf
Alexander <kleshnin.alexander@gmail.com>
2021-05-20 13:30:08 +0300

    init

---
+++ precache-manifest.8d0d946bcf6a7ebe85ab795280bd5afe.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed3fbc57b0deba884d2",
+    "url": "/np.mapillary-2021/css/app.475738c0.css"
+  },
+  {
+    "revision": "2a3d8440484c9afbcedc",
+    "url": "/np.mapillary-2021/css/chunk-vendors.487610a0.css"
+  },
+  {
+    "revision": "7bb67099bb0d97163d08c9b92b685e42",
+    "url": "/np.mapillary-2021/favicon.png"
+  },
+  {
+    "revision": "7bb67099bb0d97163d08c9b92b685e42",
+    "url": "/np.mapillary-2021/img/mapillary_logo.7bb67099.png"
+  },
+  {
+    "revision": "de2944995ea6c57df82e521e70898e24",
+    "url": "/np.mapillary-2021/img/osm_logo.de294499.png"
+  },
+  {
+    "revision": "020d7e85b2d34eac2e6c3baf925e20f8",
+    "url": "/np.mapillary-2021/img/scgis_logo.020d7e85.png"
+  },
+  {
+    "revision": "cb4ab6bf47e0900d17281eafdac96a94",
+    "url": "/np.mapillary-2021/index.html"
+  },
+  {
+    "revision": "8ed3fbc57b0deba884d2",
+    "url": "/np.mapillary-2021/js/app.01a83ac8.js"
+  },
+  {
+    "revision": "2a3d8440484c9afbcedc",
+    "url": "/np.mapillary-2021/js/chunk-vendors.57be50fb.js"
+  },
+  {
+    "revision": "2c737fe88db85e2f87f0",
+    "url": "/np.mapillary-2021/js/notfound.d67b3461.js"
+  },
+  {
+    "revision": "6f1947084aaaa1a6a90db660bbe5efc6",
+    "url": "/np.mapillary-2021/manifest.json"
+  },
+  {
+    "revision": "5b25769a2b4f996d556876801c082dd7",
+    "url": "/np.mapillary-2021/mapillary_data.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/np.mapillary-2021/robots.txt"
+  },
+  {
+    "revision": "a302b20f9eea7d70102dd642d8d25ff2",
+    "url": "/np.mapillary-2021/upd.json"
+  }
+]);
